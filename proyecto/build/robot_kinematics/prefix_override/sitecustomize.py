562a1fbe16dbf8c4d9b69ef66b364761fdcbbfa0
DesireSM <desirejacobfi@gmail.com>
2026-06-09 21:07:48 -0600

    URDF LISTO, CINEMÁTICA CASI LISTA, VISUALIZACIÓN EN RVIZ Y MOVIMIENTO CON CLICK

---
+++ proyecto/build/robot_kinematics/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/robotics/Prácticas/Robotica2026-2/proyecto_ws/install/robot_kinematics'
---
+++ proyecto/src/robot_description/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from glob import glob
+package_name = 'robot_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + "/rviz", ["rviz/rviz.conf.rviz"]),
+        ('share/' + package_name + "/urdf", ["urdf/robot.urdf"]),
+        # Para incluir todos los archivos dentro de una carpeta con un solo comando
+        ('share/' + package_name + "/meshes", glob('meshes/*')) 
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robotics',
+    maintainer_email='roberto.gar.1748@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
