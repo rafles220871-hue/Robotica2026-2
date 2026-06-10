f800de8ddac70a95d1f8d4aff801e6a91ed88802
DesireSM <desirejacobfi@gmail.com>
2026-06-09 21:21:41 -0600

    URDF LISTO, VISUALIZACIÓN EN RVIZ, MOVIMIENTO CON CLICKS, CINEMÁTICA CASI LISTA.

--- proyecto/build/robot_description/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/robotics/Prácticas/Robotica2026-2/proyecto_ws/install/robot_description'
